8b9e183f58405cfcf9595ad9b46515d838e94bc3
MayaSelena773 <maya.bono721@gmail.com>
2022-08-30 19:16:12 +0200

    first commit

---
+++ database/migrations/2022_08_29_155019_create_trips_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 70);
+            $table->string('destination', 150);
+            $table->date('start_date');
+            $table->tinyInteger('days')->unsigned();
+            $table->decimal('price', $precision = 8, $scale = 2);
+            $table->string('vehicle', 20);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
